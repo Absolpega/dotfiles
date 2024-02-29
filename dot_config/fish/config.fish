b0747fa25c7851d937b2864c94c9e5178c303ca6
Absolpega <106615943+Absolpega@users.noreply.github.com>
2024-02-29 11:53:01 +0100

    Update deps.txt Update .config/fish/config.fish Update .config/foot/foot.ini Update .config/hypr/hyprland.conf Update .config/hypr/settings.conf Update .config/rofi/black_launcher.rasi Update .config/swaync/style.css Update .config/swayosd/style.css Update .config/waybar/config Update .config/waybar/style.css

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,60 +1,29 @@
 if status is-interactive
 
-    # -----------------------------------------------------------------------------------------------------
-
-    fish_vi_key_bindings
+    fish_default_key_bindings
+    #fish_vi_key_bindings
 
     fish_add_path ~/.local/bin ~/.cargo/bin ~/.ghcup/bin ~/.luarocks/bin/
 
+    starship init fish | source
+    zoxide init --cmd cd fish | source
+    source /usr/share/nnn/quitcd/quitcd.fish
+
     export VISUAL=nvim
     export EDITOR=nvim
-
-    # -----------------------------------------------------------------------------------------------------
+    export GOPATH=.go
 
     alias ls='dirs; eza -L 1 -T --icons'
     alias l=ls
     alias la='ls -hal'
     alias lt='eza -T --icons'
 
-    alias j=z
+    alias rm=trash
 
     alias reload='source ~/.config/fish/config.fish'
 
-    export SUDO_ASKPASS="$(which lua-askpass 2> /dev/null || which ~/.luarocks/bin/askpass.lua 2> /dev/null)"
-    alias sudo='sudo -A'
-
-    function rm
-
-    end
-
-    # -----------------------------------------------------------------------------------------------------
-
-    starship init fish | source
-
-    zoxide init fish | source
-
-    source /usr/share/nnn/quitcd/quitcd.fish
-
-
-    # this just renames starship's prompt so i can have my own fish_prompt
-    # needs to be erased before being copied
-    functions --erase starship_prompt
-    functions --copy fish_prompt starship_prompt
-
-    functions --erase starship_right_prompt
-    functions --copy fish_right_prompt starship_right_prompt
-
-
-    # -----------------------------------------------------------------------------------------------------
-
-    # these need to be after starship
-    function fish_prompt
-        starship_prompt
-    end
-
-    function fish_right_prompt
-        starship_right_prompt
-    end
+    export SUDO_ASKPASS="$(which askpass.lua 2> /dev/null)"
+    [ -n $SUDO_ASKPASS ] && alias sudo='sudo -A'
 
     function fish_greeting
         # https://github.com/Absolpega/fetch
