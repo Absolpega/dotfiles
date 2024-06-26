61abbba80ef60d27c836b89adff3c0e1eaf18626
Absolpega <106615943+Absolpega@users.noreply.github.com>
2024-06-26 19:38:46 +0200

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     fish_default_key_bindings
     #fish_vi_key_bindings
 
-    fish_add_path ~/.local/bin ~/.cargo/bin ~/.luarocks/bin/
+    fish_add_path -P ~/.local/bin ~/.cargo/bin ~/.luarocks/bin/
 
     starship init fish | source
     zoxide init --cmd cd fish | source
@@ -10,22 +10,23 @@ if status is-interactive
     export VISUAL=nvim
     export EDITOR=nvim
 
+    abbr l ls
+    abbr rm trash
+
     alias ltree='eza -T --icons'
     alias ls='ltree -L 1'
-    alias l=ls
-
-    alias rm=trash
-
     alias reload='source ~/.config/fish/config.fish'
-
     alias dotfiles='command git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"'
 
     # https://github.com/Absolpega/lua-askpass
-    export SUDO_ASKPASS="$(which askpass.lua 2> /dev/null)"
+    #set askpass askpass.lua
+    set askpass systemd-ask-password
+    export SUDO_ASKPASS="$(which $askpass 2> /dev/null)"
     [ -n $SUDO_ASKPASS ] && alias sudo='sudo -A'
 
     function fish_greeting
-        # https://github.com/Absolpega/fetch
-        command -v fetch >/dev/null && fetch || true
+        # https://github.com/Absolpega/lffetch
+        set lffetch "$(which lffetch.lua 2> /dev/null)"
+        [ -n $lffetch ] && lffetch.lua
     end
 end
